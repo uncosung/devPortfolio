dea468d9650c9ca114fb0873b8a9ab5f13f7f333
uncosung <eric.k.sung@gmail.com>
2019-09-29 23:42:20 +0000

    updated dev profile

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(startApp);
+
+function startApp () {
+    imageHover();
+    scrollTop();
+    $('#scroll-up').on('click', () => {
+        $('html, body').stop().animate({
+            scrollTop:0
+        }, 500, 'linear');
+        return false
+    });
+}
+
+function imageHover () {
+    $('.memory').hover(() => {
+        $('.memory-details').toggleClass('hidden');
+        $('.memory img').toggleClass('hovered');
+    })
+    $('.sales').hover(() => {
+        $('.sales-details').toggleClass('hidden');
+        $('.sales img').toggleClass('hovered');
+    })
+    $('.tuur').hover(() => {
+        $('.tuur-details').toggleClass('hidden');
+        $('.tuur img').toggleClass('hovered');
+    })
+}
+function scrollTop () {
+    var button = $('#scroll-up');
+    var timeoutKey = -1;
+    $(document).on('scroll', () => {
+        if(timeoutKey) {
+            window.clearTimeout(timeoutKey);
+        }
+        timeoutKey = window.setTimeout( () => {
+            if($(window).scrollTop() < 100) {
+                button.fadeOut();
+            }
+            else {
+                button.fadeIn();
+            }
+        }, 100);
+    })
+}
